4eb5c23593df6c3e2cb169531c262c85f634dcf6
Brinkhuis <rene.brinkhuis@gmail.com>
2017-12-09 14:02:40 +0100

    Code optimization fir scraping Fit For Free website

--- code/data_collection.py
+++ code/data_collection.py
@@ -6,20 +6,21 @@ Scrape data on gym locations
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
+from tqdm import tqdm
 
 # scrape basic-fit
 URL = 'https://www.basic-fit.com/nl-nl/sportscholen'
-SOUP = BeautifulSoup(requests.get(URL).text, 'html.parser')
+SOUP = BeautifulSoup(requests.get(URL).content, 'lxml')
 GYM_URL = list()
 LINKS = SOUP.find_all('a')
-for link in LINKS:
+for link in tqdm(LINKS):
     if link['href'].startswith('/nl-nl/sportscholen/'):
         GYM_URL.append('https://www.basic-fit.com' + link['href'])
 GYM_URL = GYM_URL[3:-6]
 
 ADDRESS = list()
-for url in GYM_URL:
-    soup = BeautifulSoup(requests.get(url).text, 'html.parser')
+for url in tqdm(GYM_URL):
+    soup = BeautifulSoup(requests.get(url).content, 'lxml')
     address = soup.find_all('li', {'class': 'active'})
     for addr in address:
         ADDRESS.append(addr.text.strip())
@@ -29,16 +30,14 @@ BF = pd.DataFrame({'company': 'Basic-Fit',
 
 # scrape fit-for-free
 URL = 'https://www.fitforfree.nl/clubs'
-SOUP = BeautifulSoup(requests.get(URL).text, 'html.parser')
-SOUP.find_all('span', {'itemprop': ['streetAddress', 'addressLocality']})
+SOUP = BeautifulSoup(requests.get(URL).content, 'lxml')
 
-STREET = list()
-for address in SOUP.find_all('span', {'itemprop': 'streetAddress'}):
-    STREET.append(address.text.replace('  ', ' ').strip())
-
-LOCALITY = list()
-for address in SOUP.find_all('span', {'itemprop': 'addressLocality'}):
-    LOCALITY.append(address.text.replace('  ', ' ').strip())
+STREET = [street.text.replace('  ', ' ').strip() for
+          street in
+          SOUP.find_all('span', {'itemprop': 'streetAddress'})]
+LOCALITY = [locality.text.replace('  ', ' ').strip() for
+            locality in
+            SOUP.find_all('span', {'itemprop': 'addressLocality'})]
 
 FFF = pd.DataFrame({'company': 'Fit For Free',
                     'address': [x[0] + ', ' + x[1] for x in list(zip(STREET, LOCALITY))]})
@@ -47,4 +46,4 @@ FFF = pd.DataFrame({'company': 'Fit For Free',
 GYM = pd.concat([BF, FFF])
 
 # save data to file
-GYM.to_csv('data/gym_raw.csv', sep=';', index=False)
+GYM.to_csv('data/gym_raw_test.csv', sep=';', index=False)
